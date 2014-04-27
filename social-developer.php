1f4ab78c37e128c0acf06363eabe0a5d5a22857d
Luke Watts <luke@luke-watts.com>
2014-04-27 18:55:53 +0100

    Add Codepen shortcode and size and link attributes

--- social-developer.php
+++ social-developer.php
@@ -23,14 +23,49 @@ function lw_sd_load_styles() {
 add_action( 'wp_enqueue_scripts', 'lw_sd_load_styles' );
 
 /**
- * Add Github shortcodes
+ * Add Github shortcode
+ *
+ * @param  $atts (required) Parses users text in sortcodes attributes
  */
 function lw_sd_github_shortcode( $atts ) {
-  // Set HTML
-  $output = '<a href="http://github.com/lukewatts"><span class="genericon genericon-github"></span></a>';
 
-  // Output HTML
+  // Attributes
+  extract( shortcode_atts(
+    
+    array(
+      'link'  => 'http://github.com',
+      'size'  => '12px'
+    ), $atts, 'lw_sd_github_shortcode' )
+
+  );
+
+  // Set HTML
+  $output = '<a href="' . $link . '"><span  style="font-size: ' . $size . ';height: ' . $size . ';width: ' . $size . ';" class="genericon genericon-github"></span></a>';
+
   return $output;
 }
 
 add_shortcode( 'sd_github', 'lw_sd_github_shortcode' );
+
+/**
+ * Add Codepen shortcode
+ */
+function lw_sd_codepen_shortcode( $atts ) {
+
+  // Attributes
+  extract( shortcode_atts(
+
+    array(
+      'link'  => 'http://codepen.io',
+      'size'  => '12px'
+    ), $atts, 'lw_sd_codepen_shortcode' )
+
+  );
+
+  // Set HTML
+  $output = '<a href="' . $link . '"><span  style="font-size: ' . $size . ';height: ' . $size . ';width: ' . $size . ';" class="genericon genericon-codepen"></span></a>';
+
+  return $output;
+}
+
+add_shortcode( 'sd_codepen', 'lw_sd_codepen_shortcode' );
