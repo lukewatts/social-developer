4f8b8cae63f5f00cf274b9dcb48299bdca99a703
Luke Watts <luke@luke-watts.com>
2014-04-28 08:53:35 +0100

    Add Quicktags for Github and Codepen shortcodes

--- social-developer.php
+++ social-developer.php
@@ -86,3 +86,27 @@ function lw_sd_codepen_shortcode( $atts ) {
 }
 
 add_shortcode( 'sd_codepen', 'lw_sd_codepen_shortcode' );
+
+
+/**
+ * Add all Quicktags
+ */
+function lw_sd_quicktags() {
+
+  if ( wp_script_is( 'quicktags' ) ) {
+  ?>
+
+  <script type="text/javascript">
+  // Github Shortcode
+  QTags.addButton( 'lw_sd_github', 'Github', '[sd_github link="https://github.com/" size="12px" new_tab="yes"]', '', 'g', 'Github Icon Shortcode', 200 );
+
+  // Codepen Shortcode
+  QTags.addButton( 'lw_sd_codepen', 'Codepen', '[sd_codepen link="http://codepen.io/" size="12px" new_tab="yes"]', '', 'p', 'Codepen Icon Shortcode', 200 );
+  </script>
+  
+  <?php
+  }
+
+}
+
+add_action( 'admin_print_footer_scripts', 'lw_sd_quicktags' );
